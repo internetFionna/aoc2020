d0186afccd9073254546d98cf4d19882fc9a5697
Fionna Adams <72765248+internetFionna@users.noreply.github.com>
2020-12-01 21:16:09 -0800

    Fininshed day 1 puzzles
    Renamed day 1.py to day1a.py, created day1b.py, added input.txt.

---
+++ day1b.py
@@ -0,0 +1,13 @@
+day1=open("input.txt","r")
+numbersRaw=day1.read().split()
+day1.close
+numbers=[]
+for num in range(199):
+    numbers.append(int(numbersRaw[num]))
+for first in range(198):
+    result = 0
+    for second in range(1, 199):
+        for third in range (2, 198):
+            if numbers[first] + numbers[second] + numbers[third] == 2020:
+                result = numbers[first] * numbers[second] * numbers[third]
+                print(result)
